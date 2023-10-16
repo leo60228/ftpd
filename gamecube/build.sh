01d0c59e89ad043251c4e45dd017f981416cc6e4
leo60228 <leo@60228.dev>
2023-10-16 17:45:42 -0400

    gamecube docker buildscript

---
+++ gamecube/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")"/..
+docker build gamecube -t libogc2
+docker run --rm -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u "$(id -u):$(id -g)" -v $PWD:$PWD -w $PWD libogc2 make -j"$(nproc)" dol
